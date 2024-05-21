80f07bd1252bb912e8b5d09ff103b0330a843060
Anay Baid <anaybaid7@gmail.com>
2024-05-21 01:32:30 -0400

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,13 +40,13 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
-    email: "johndoe@mail.com",
+    title: "Anay Baid's Portfolio - University of Waterloo - 2B Computer Science",
+    fullName: "Anay Baid",
+    email: "anay.baid@uwaterloo.ca",
   },
   hero: {
-    name: "John Doe",
-    p: ["I develop 3D visuals, user", "interfaces and web applications"],
+    name: "Anay Baid",
+    p: ["I am deeply passionate about software development and continuously strive to stay up-to-date with the latest advancements in technology."],
   },
   contact: {
     p: "Get in touch",
@@ -67,11 +67,10 @@ export const config: TConfig = {
     about: {
       p: "Introduction",
       h2: "Overview.",
-      content: `I'm a skilled software developer with experience in TypeScript and
-      JavaScript, and expertise in frameworks like React, Node.js, and
-      Three.js. I'm a quick learner and collaborate closely with clients to
-      create efficient, scalable, and user-friendly solutions that solve
-      real-world problems. Let's work together to bring your ideas to life!`,
+      content: `Hi there,
+      I'm Anay Baid, currently pursuing my degree in Computer Science at the UWaterloo. Throughout my academic journey, I've been deeply immersed in the realms of data engineering and software development, continually expanding my skill set and knowledge base.
+      During my internships, I've had the privilege of engaging in hands-on projects that have allowed me to sharpen my abilities in crucial areas such as data analysis and system optimization. 
+      I'm genuinely eager to leverage my experiences and capabilities to make meaningful contributions to your projects. Let's connect and discuss how I can be an asset to your team!`,
     },
     experience: {
       p: "What I have done so far",
@@ -84,7 +83,7 @@ export const config: TConfig = {
     works: {
       p: "My work",
       h2: "Projects.",
-      content: `Following projects showcases my skills and experience through
+      content: `The following projects showcase my skills and experience through
     real-world examples of my work. Each project is briefly described with
     links to code repositories and live demos in it. It reflects my
     ability to solve complex problems, work with different technologies,
