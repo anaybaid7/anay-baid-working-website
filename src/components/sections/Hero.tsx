8dccbeff1cae9ea3432084d85b331005bc7add70
Anay Baid <anaybaid7@gmail.com>
2024-05-27 22:22:45 -0400

    Update Hero.tsx

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       >
         <div className="mt-5 flex flex-col items-center justify-center">
           <div className="h-5 w-5 rounded-full bg-[#915EFF]" />
-          <div className="violet-gradient h-40 w-1 sm:h-80" />
+          <div className="pink-gradient h-40 w-1 sm:h-80" />
         </div>
 
         <div>
